b63d7ffde91e2b37b0a25c5f34a75e28ca477df8
noramohamsa <nora.mohamed231@gmail.com>
2024-09-25 19:55:25 +0300

    Updating data file

--- src/components/logossection/Logos.jsx
+++ src/components/logossection/Logos.jsx
@@ -1,15 +1,7 @@
-function Logos() {
-  const logos = [
-    "https://cms-images.udemycdn.com/content/tqevknj7om/svg/volkswagen_logo.svg?position=c&quality=80&x.app=portals",
-    "https://cms-images.udemycdn.com/content/2gevcc0kxt/svg/samsung_logo.svg?position=c&quality=80&x.app=portals",
-    "https://cms-images.udemycdn.com/content/mueb2ve09x/svg/cisco_logo.svg?position=c&quality=80&x.app=portals",
-    "https://cms-images.udemycdn.com/content/ryaowrcjb2/svg/vimeo_logo_resized-2.svg?position=c&quality=80&x.app=portals",
-    "https://cms-images.udemycdn.com/content/bthyo156te/svg/procter_gamble_logo.svg?position=c&quality=80&x.app=portals",
-    "https://cms-images.udemycdn.com/content/luqe0d6mx2/svg/hewlett_packard_enterprise_logo.svg?position=c&quality=80&x.app=portals",
-    "https://cms-images.udemycdn.com/content/siaewwmkch/svg/citi_logo.svg?position=c&quality=80&x.app=portals",
-    "https://cms-images.udemycdn.com/content/swmv0okrlh/svg/ericsson_logo.svg?position=c&quality=80&x.app=portals",
-  ];
+import { logos } from "../../data/Logosdata";
 
+function Logos() {
+ 
   return (
     <div className="w-full bg-slate-100 border-red-50 h-auto">
       <div className="w-10/12 mx-auto px-2 flex flex-col justify-center">
